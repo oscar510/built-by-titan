e54dca865b1a565c89cc339f1af348cf748df544
oscar <oscarbahai510@gmail.com>
2015-01-29 19:59:33 -0800

    three circles

--- oscar01.py
+++ oscar01.py
@@ -1,46 +1,60 @@
 import turtle
 
-square_size = 100
-
-turtle.forward(square_size)
-
-
-
-turtle.right(90)
-
-turtle.forward(square_size)
-
-turtle.right(90)
-
-
-turtle.forward(square_size)
+def draw_square():
+    square_size = 100
+    turtle.forward(square_size)
+    turtle.right(90)
+    turtle.forward(square_size)
+    turtle.right(90)
+    turtle.forward(square_size)
+    turtle.right(90)
+    turtle.forward(square_size)
 
 
 
-turtle.right(90)
+
+def draw_circles():
+    turtle.back(60)
+    turtle.left(90)
+    turtle.left(90)
+    turtle.left(90)
+    turtle.pu()
+    turtle.forward(50)
+    turtle.pd()
+    turtle.circle(10)
+    turtle.pu()
+    turtle.back(50)
+    turtle.right(90)
+    turtle.forward(20)
+    turtle.left(90)
+    turtle.forward(25)
+    turtle.pd()
+    turtle.circle(10)
+    turtle.pu()
+    turtle.back(25)
+    turtle.left(90)
+    turtle.forward(80)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(20)
+    turtle.right(90)
+    turtle.forward(25)
+    turtle.pd()
+    turtle.circle(10)
+
+draw_square()
+
+draw_circles()
 
 
 
-turtle.forward(square_size)
+
+
+               
 
 
 
-turtle.back(60)
-
-turtle.left(90)
-
-turtle.left(90)
-
-turtle.left(90)
-
-turtle.pu()
-
-
-turtle.forward(50)
-
-turtle.pd()
-
-turtle.circle(10)
 
 
 
